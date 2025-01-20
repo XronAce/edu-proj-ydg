00ba47c4f9ab5bb6b8cc94b0da29dcb1715a7cc5
DonggiYang <xronace@gmail.com>
2025-01-20 22:09:15 +0900

    refactor: Erase unused imports

--- backend/edu-user/src/main/java/com/ktds/eduuser/service/UserService.java
+++ backend/edu-user/src/main/java/com/ktds/eduuser/service/UserService.java
@@ -5,7 +5,6 @@ import com.ktds.eduuser.dto.response.GetUserResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
